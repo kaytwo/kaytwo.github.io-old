bc2cb1f4bdad4bb5bf5d69112a54917eb6621398
Chris Kanich <ckanich@uic.edu>
2016-10-30 22:53:23 -0500

    favicon, scroll hack

--- js/grayscale.js
+++ js/grayscale.js
@@ -35,8 +35,6 @@ $('.navbar-collapse ul li a').click(function() {
 // Google Maps Scripts
 var map = null;
 var ctr = new google.maps.LatLng(41.8971247,-87.6211848);
-var nearbyMap = null;
-var nearbyCtr = new google.maps.LatLng(41.8971247,-87.6211848);
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
@@ -82,44 +80,30 @@ function init() {
         map: map,
         title:"Museum of Contemporary Art"
     });
-
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 15,
-
-        // The latitude and longitude to center the map (always required)
-        center: nearbyCtr, // New York
-
-        // Disables the default Google Maps UI components
-        scrollwheel: false,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-            "elementType": "labels.icon",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }]
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('nearbymap');
-
-    // Create the Google Map using out element and options defined above
-    nearbyMap = new google.maps.Map(mapElement, mapOptions);
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var myLatLng = nearbyCtr;
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: nearbyMap,
-        title:"Museum of Contemporary Art"
-    });
-
 }
 
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function (event) {
+  var that = $(this);
 
+  that.on('click', onMapClickHandler);
+  that.off('mouseleave', onMapMouseleaveHandler);
+  that.find('iframe').css("pointer-events", "none");
+}
+
+var onMapClickHandler = function (event) {
+  var that = $(this);
+
+  // Disable the click handler until the user leaves the map area
+  that.off('click', onMapClickHandler);
+
+  // Enable scrolling zoom
+  that.find('iframe').css("pointer-events", "auto");
+
+  // Handle the mouse leave event
+  that.on('mouseleave', onMapMouseleaveHandler);
+}
+
+// Enable map zooming with mouse scroll when the user clicks the map
+$('#mapoverlay').on('mouseover',
+  onMapClickHandler);
